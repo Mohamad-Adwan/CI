6d6fc0b42f67761852c9a62bd8c942d5c827bf4f
Mohammad <mohammad@gmail.com>
2026-02-26 13:23:20 +0200

    Serve Welcome to Notely from HTML file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-res.end('Welcome to Notely');
+  const filePath = path.join(__dirname, 'assets', 'index.html');
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading page');
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(data);
+  });
 });
 
 const port = process.env.PORT || 8080;
 server.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
 });
-
